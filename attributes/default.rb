73460f792be006713a05e6a83c8e3e4d8f500695
Anshul <optimisticanshul@gmail.com>
2016-02-18 18:31:24 +0530

    Updated Config

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,7 +15,7 @@ when 'centos'
   default['pgpool']['service'] = 'pgpool'
 end
 
-default['pgpool']['pgconf']['listen_addresses'] = 'localhost'
+default['pgpool']['pgconf']['listen_addresses'] = '*'
 default['pgpool']['pgconf']['port'] = 9999
 default['pgpool']['pgconf']['pcp_port'] = 9898
 default['pgpool']['pgconf']['pool_password'] = 'pool_passwd'
@@ -27,7 +27,7 @@ default['pgpool']['pgconf']['child_life_time'] = 300
 default['pgpool']['pgconf']['child_max_connections'] = 0
 default['pgpool']['pgconf']['connection_life_time'] = 0
 default['pgpool']['pgconf']['client_idle_limit'] = 0
-default['pgpool']['pgconf']['enable_pool_hba'] = false # Might want to override
+default['pgpool']['pgconf']['enable_pool_hba'] = true # Might want to override
 # this if the use case includes authentication by Pgpool (recommended).
 default['pgpool']['pgconf']['logdir'] = '/var/log/pgpool'
 default['pgpool']['pgconf']['connect_timeout'] = 10_000
@@ -45,8 +45,8 @@ default['pgpool']['pgconf']['load_balance_mode'] = true
 default['pgpool']['pgconf']['backend_weight0'] = 1
 default['pgpool']['pgconf']['backend_data_directory0'] = '/var/lib/pgpool/master/data'
 default['pgpool']['pgconf']['backend_data_directory1'] = '/var/lib/pgpool/slave/data'
-default['pgpool']['pgconf']['backend_flag0'] = 'ALLOW_TO_FAILOVER'
-default['pgpool']['pgconf']['backend_flag1'] = 'ALLOW_TO_FAILOVER'
+default['pgpool']['pgconf']['backend_flag0'] = 'DISALLOW_TO_FAILOVER'
+default['pgpool']['pgconf']['backend_flag1'] = 'DISALLOW_TO_FAILOVER'
 default['pgpool']['pgconf']['authentication_timeout'] = 60
 default['pgpool']['pgconf']['child_life_time'] = 300
 default['pgpool']['pgconf']['child_max_connections'] = 0 # Should override
@@ -56,17 +56,17 @@ default['pgpool']['pgconf']['log_destination'] = 'syslog'
 default['pgpool']['pgconf']['syslog_facility'] = 'LOCAL0'
 default['pgpool']['pgconf']['syslog_ident'] = 'pgpool'
 default['pgpool']['pgconf']['print_timestamp'] = true
-default['pgpool']['pgconf']['log_connections'] = false # Should override
-default['pgpool']['pgconf']['log_hostname'] = false # Should override possibly
-default['pgpool']['pgconf']['log_statement'] = false
-default['pgpool']['pgconf']['log_per_node_statement'] = false
+default['pgpool']['pgconf']['log_connections'] = true # Should override
+default['pgpool']['pgconf']['log_hostname'] = true # Should override possibly
+default['pgpool']['pgconf']['log_statement'] = true
+default['pgpool']['pgconf']['log_per_node_statement'] = true
 default['pgpool']['pgconf']['log_standby_delay'] = 'if_over_threshold'
 default['pgpool']['pgconf']['debug_level'] = 0 # Probably want to override
 default['pgpool']['pgconf']['pid_file_name'] = '/var/run/pgpool/pgpool.pid'
 default['pgpool']['pgconf']['logdir'] = '/var/log/pgpool'
 default['pgpool']['pgconf']['connection_cache'] = true
 default['pgpool']['pgconf']['reset_query_list'] = 'ABORT; DISCARD ALL'
-default['pgpool']['pgconf']['master_slave_mode'] = false
+default['pgpool']['pgconf']['master_slave_mode'] = true
 default['pgpool']['pgconf']['master_slave_sub_mode'] = 'stream'
 default['pgpool']['pgconf']['sr_check_period'] = 10
 default['pgpool']['pgconf']['sr_check_user'] = 'pgpool' # Should override
@@ -121,6 +121,7 @@ default['pgpool']['pgconf']['other_pgpool_hostname0'] = '' # Should override if
 # HA Pgpool is desired.
 default['pgpool']['pgconf']['other_pgpool_port0'] = 9999
 default['pgpool']['pg_hba']['auth'] = [
-  { type: 'local', db: 'all', user: 'all', addr: nil, method: 'trust' },
-  { type: 'host', db: 'all', user: 'all', addr: '127.0.0.1/32', method: 'trust' }
+  { type: 'local', db: 'all', user: 'all', addr: nil, method: 'md5' },
+  { type: 'local', db: 'all', user: 'all', addr: '0.0.0.0/0', method: 'md5' },
+  { type: 'host', db: 'all', user: 'all', addr: '127.0.0.1/32', method: 'md5' }
 ]
